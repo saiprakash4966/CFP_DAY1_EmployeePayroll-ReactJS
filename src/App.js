d0265ed0f6ee8e8a43691411df4ab902a332c510
Your Name <you@example.com>
2022-04-26 18:58:28 +0530

    Displaying bridglabz logo using Property data binding

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.jpg';
+import React from 'react';
+import './App.css';
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      title:'hello from Bridgelabz'
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+        <img src={logo} alt="bridgelabz logo"/>
+      </div>
+    );
+  }
+}
+
+export default App;
