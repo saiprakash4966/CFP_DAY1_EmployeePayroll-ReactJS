823d2dfac04aaf44e1841febcf884dbf6882059a
Your Name <you@example.com>
2022-04-27 11:01:32 +0530

    Launching bridgelabz site in a new tab on clicking bridgelabz logo

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component{
+  url = 'https://www.bridgelabz.com/'
   constructor(){
     super();
     this.state={
       title:'hello from Bridgelabz'
     }
   }
+ //onclick function
+ onClick=($event) =>{
+   console.log('save button is clicked',$event);
+   window.open(this.url,"_blank");
+ }
   render() {
     return (
       <div>
         <h1>{this.state.title}</h1>
-        <img src={logo} alt="bridgelabz logo"/>
+        <img src={logo} onClick={this.onClick} alt="bridgelabz logo"/>
       </div>
     );
   }
