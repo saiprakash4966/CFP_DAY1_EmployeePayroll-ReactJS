8c3791dc26d47a75b7721452d7210556b5e1eb26
Your Name <you@example.com>
2022-04-27 11:19:07 +0530

    Displaying hello from bridgeklabz using user input name

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import logo from './logo.jpg';
-import React from 'react';
 import './App.css';
+import React from 'react';
 
-class App extends React.Component{
+class App extends React.Component {
   url = 'https://www.bridgelabz.com/'
-  constructor(){
-    super();
-    this.state={
-      title:'hello from Bridgelabz'
+  constructor() {
+    super()
+    this.state = {
+    userName: ''
     }
   }
- //onclick function
- onClick=($event) =>{
-   console.log('save button is clicked',$event);
-   window.open(this.url,"_blank");
- }
+
+  // onClick function
+  onClick = ($event) => {
+    console.log('save button is clicked', $event);
+    window.open(this.url, "_blank");
+  }
+
+  onNameChnage = (event) => {
+    console.log("value is ", event.target.value);
+    // set the title using setState method
+    this.setState({ userName: event.target.value })
+  }
+
   render() {
     return (
+      <>
       <div>
-        <h1>{this.state.title}</h1>
-        <img src={logo} onClick={this.onClick} alt="bridgelabz logo"/>
+        <h1>{this.state.userName}</h1>
+        <img src={logo} onClick={this.onClick}
+          alt="This Bridgelabz logo: a Bridge to Employment through lab works" />
       </div>
+      <div>
+        <input onChange={this.onNameChnage}/>
+      </div>
+      </>
     );
   }
 }
