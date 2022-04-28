252424331deadc4ae11d4588ab151f83b4248661
Your Name <you@example.com>
2022-04-28 10:24:10 +0530

    Creating Home Component to display hello from bridgelabz page

--- src/App.js
+++ src/App.js
@@ -1,77 +1,82 @@
-import logo from './logo.jpg';
-import './App.css';
-import React from 'react';
+import logo from "./assets/BL-logo.jpg";
+import React from "react";
+import "./App.css";
+import Home from "./Home.js";
+import About from "./About";
+import { Switch, Link, Route } from "react-router-dom";
 
 class App extends React.Component {
-  url = 'https://www.bridgelabz.com/'
+  url = "https://www.bridgelabz.com/";
   constructor() {
-    super()
+    super();
     this.state = {
-      userName: '',
-      nameError: ''
-    }
+      //title: 'Hello from Bridgelabz',
+      userName: "",
+      nameError: "",
+    };
   }
 
-  // onClick function
-  onClick = ($event) => {
-    console.log('save button is clicked', $event);
-    window.open(this.url, "_blank");
-  }
+  /**
+   * onClick method is called when image is clicked
+   */
+  onClick = () => {
+    window.open(this.url);
+  };
 
-  onNameChnage = (event) => {
-    console.log("value is ", event.target.value);
-    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
-    // set the title using setState method
-    this.setState({ userName: event.target.value })
+  onNameChange = (event) => {
+    const nameRegex = RegExp("^[A-Z]{1}[a-zA-Z\\s]{2,}$");
+    this.setState({ userName: event.target.value });
     if (nameRegex.test(event.target.value)) {
-      this.setState({ nameError: '' })
+      this.setState({ nameError: "" });
     } else {
-      this.setState({ nameError: 'Name is Incorrect' })
+      this.setState({ nameError: "Invalid NAME" });
     }
-  }
+  };
 
   render() {
     return (
-      <>
-        <html>
-          <body>
-            <div className="container">
-              <div className="main-container">
-                <div className="header">
-                  <h1>Hello {this.state.userName} from Bridgelabz</h1>
-                </div>
-                <div>
-                  <img src={logo} onClick={this.onClick}
-                    alt="This Bridgelabz logo: a Bridge to Employment through lab works" />
-                  <input onChange={this.onNameChnage} />
-                </div>
-                <span className="error-output">{this.state.nameError}</span>
-                <div>
-                  <p>At BridgeLabz, we're a community of </p>
-                  <ul>
-                    <li>Technoligist</li>
-                    <li>Thinkers</li>
-                    <li>Builders</li>
-                  </ul>
-                  <p>
-                    Working together to keep Tech employability of enginners alive and
-                    accesible so tech comanies worldwide
-                    get contributors and creators for technology solutions.
-                    We believe this act of human collaboration acrosss an employability
-                    platform is essential to individual growth and our collective future
-                  </p>
-                  <p>
-                    To know about us , visit <a href="https://www.bridgelabz.com/">BridgeLabz</a>
-                    to learn even more about out mission i.e. <strong>Employability to all</strong>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </body>
-        </html>
-      </>
+      <div class="page-content">
+        <h1>Hello {this.state.userName} from BridgeLabz</h1>
+        <img
+          src={logo}
+          onClick={this.onClick}
+          alt="BridgeLabz Logo goes here"
+        />
+        <div class="input-content">
+          <input onChange={this.onNameChange} />
+          <span className="error-output">{this.state.nameError}</span>
+        </div>
+        <p>At BridgeLabz, we are a tech community of</p>
+        <ul>
+          <li>technologists</li>
+          <li>thinkers</li>
+          <li>builders</li>
+        </ul>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged.
+        </p>
+        <p>
+          To know about us, visit <a href={this.url}>BridgeLabz </a>
+          to learn even more about our misiion i.e.{" "}
+          <strong>Employability to all</strong>.
+        </p>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <Switch>
+          <Route exact path="/home" component={Home}></Route>
+          <Route exact path="/about" component={About}></Route>
+        </Switch>
+      </div>
     );
   }
 }
-
 export default App;
